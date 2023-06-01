bbd6bc8e886e434a6cd65bf65d301289c1593679
semyonf1l1pp0v <syomchik13@gmail.com>
2023-06-01 22:18:19 +0300

    universal func parse_description added for beer region, type and style

--- parse_data.py
+++ parse_data.py
@@ -20,38 +20,27 @@ soup = BeautifulSoup(src, "lxml")
 
 info = soup.find_all("ul", class_="list-description")
 
-# # GETTING AND PRINTING beer region
+
+# GETTING AND PRINTING beer parameter based on variable param_num
+# def parse_description(param_num):
+#     for el in info:
+#         information = el.find_all("li")
+#         parameter = str(information[param_num].text).split(":")[1:]  # same idea like lines 19-22
+#         parameter_corrected = ''  # will also appear in functions below
+#         for par in parameter:
+#             parameter_corrected += par.replace('\n', '')
+#         print(parameter_corrected)
+
+
+# parse_description(0)  # beer_region
+# parse_description(3)  # beer_type
+# parse_description(4)  # beer_style
+
+# # GETTING AND PRINTING beer strength
 # for el in info:
 #     information = el.find_all("li")
-#     region = str(information[0].text,).split(":")[1:]     # same idea like lines 19-22
-#     region_corrected = ''                                           # will also appear in functions below
-#     for reg in region:
-#         region_corrected += reg.replace('\n', '')
-#     print(region_corrected)
-
-# # GETTING AND PRINTING beer type
-# for el in info:
-#     information = el.find_all("li")
-#     beer_type = str(information[3].text).split(":")[1:]
-#     beer_type_corrected = ''
-#     for ch in beer_type:
-#         beer_type_corrected += ch.replace('\n', '')
-#     print(beer_type_corrected)
-
-# # GETTING AND PRINTING beer style
-# for el in info:
-#     information = el.find_all("li")
-#     beer_style = str(information[4].text).split(":")[1:]       # same idea like lines 19-22
-#     beer_style_corrected = ''
-#     for ch in beer_style:
-#         beer_style_corrected += ch.replace('\n', '')
-#     print(beer_style_corrected)
-
-# GETTING AND PRINTING beer strength
-for el in info:
-    information = el.find_all("li")
-    beer_strength = str(information[6].text).split(":")[1:]       # same idea like lines 19-22
-    beer_strength_corrected = ''
-    for nums in beer_strength:
-        beer_strength_corrected += nums.split("-")[1].replace("%", "")
-    print(float(beer_strength_corrected))
+#     beer_strength = str(information[6].text).split(":")[1:]       # same idea like lines 19-22
+#     beer_strength_corrected = ''
+#     for nums in beer_strength:
+#         beer_strength_corrected += nums.split("-")[1].replace("%", "")
+#     print(float(beer_strength_corrected))
