6dca8b2f819bfa7aacea9fbb8c9e6c22c1281824
semyonf1l1pp0v <syomchik13@gmail.com>
2023-06-24 23:20:51 +0300

    telegram bot user input functions in progress

--- telegram_bot.py
+++ telegram_bot.py
@@ -9,7 +9,43 @@ dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
-    await message.answer("Добрый вечерочек. Какой пивандесик интересует?")
+    await message.answer("Добрый вечерочек. Давайте подберем пивчик для вас!")
+
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def get_user_beer_region(message: types.Message):
+    await message.answer("Итак, для начала нужно определиться с регионом \
+                        (Германия, Россия...)\n Введите регион:")
+    beer_region = message.text
+    await message.answer("Отлично, идем дальше...")
+    return beer_region.lower()
+
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def get_user_beer_type(message: types.Message):
+    await message.answer("Отлично, следующий вопрос: светлое или темное?\n Ваш выбор:")
+    beer_type = message.text
+    await message.answer("Принято. Осталось совсем немного!")
+    return beer_type.lower() if beer_type.lower() == "светлое" else "темное"
+
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def get_user_beer_style(message: types.Message):
+    await message.answer("Глобально пиво можно разделить на три категории:\
+                        эль, лагер или смешанное\n Что предпочтете сегодня?:")
+    beer_style = message.text
+    await message.answer("Мы уже на финишной прямой нашего пивного забега...")
+    return beer_style.lower()
+
+
+@dp.message_handler(content_types=types.ContentType.TEXT)
+async def get_user_beer_price_interval(message: types.Message):
+    await message.answer("Глобально пиво можно разделить на три категории:\
+                        эль, лагер или смешанное\n Что предпочтете сегодня?:")
+    beer_style = message.text
+    await message.answer("Мы уже на финишной прямой нашего пивного забега...")
+    return beer_style.lower()
+
 
 # ("Название", "Регион", "Тип", "Стиль", "Крепость", "Цена со скидкой", "Цена без скидки", "Объем")
 @dp.message_handler(commands="allbeer")
