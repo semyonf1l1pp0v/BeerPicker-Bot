9fd186f33d32c316cc17756e4a301f91853c799e
semyonf1l1pp0v <syomchik13@gmail.com>
2023-06-26 14:19:18 +0300

    added: random sleep, message for 0 recommendations

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,6 +5,8 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.utils.markdown import hbold
 from config import token
 from connection import collect_beer
+import random
+from time import sleep
 
 bot = Bot(token=token, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
@@ -25,6 +27,7 @@ class UserInput(StatesGroup):
 async def start(message: types.Message):
     await UserInput.waiting_for_region_input.set()
     await message.answer("Добрый вечерочек. Давайте подберем пивчик для вас!")
+    sleep(random.randint(1, 2))
     await bot.send_message(message.chat.id,
                            "Итак, для начала нужно определиться с регионом (Германия, Россия...)\nВведите регион:")
 
@@ -36,6 +39,7 @@ async def get_user_beer_region(message: types.Message, state: FSMContext):
     await UserInput.next()
     await message.answer("Отлично, идем дальше...")
     await state.update_data(beer_region=beer_region)
+    sleep(random.randint(1, 2))
     await bot.send_message(message.chat.id,
                            "Хорошо, следующий вопрос: светлое или темное?\nВаш выбор:")
 
@@ -47,7 +51,9 @@ async def get_user_beer_type(message: types.Message, state: FSMContext):
     await UserInput.next()
     await message.answer("Принято. Осталось совсем немного!")
     await state.update_data(beer_type=beer_type)
-    await bot.send_message(message.chat.id, "Глобально пиво можно разделить на три категории: эль, лагер или смешанное\nЧто предпочтете сегодня?:")
+    sleep(random.randint(1, 2))
+    await bot.send_message(message.chat.id,
+                           "Глобально пиво можно разделить на три категории: эль, лагер или смешанное\nЧто предпочтете сегодня?:")
 
 
 # Wait for user to write beer type
@@ -57,6 +63,7 @@ async def get_user_beer_style(message: types.Message, state: FSMContext):
     await UserInput.next()
     await state.update_data(beer_style=beer_style)
     await message.answer("Мы уже на финишной прямой нашего пивного забега...")
+    sleep(random.randint(1, 2))
     await bot.send_message(message.chat.id, "Введите минимальную сумму, которую вы готовы потратить (только число):")
 
 
@@ -77,6 +84,7 @@ async def get_user_beer_price_high(message: types.Message, state: FSMContext):
     await state.update_data(beer_price_high=beer_price_high)
 
     await bot.send_message(message.chat.id, "Щас посмотрим че у нас тут есть")
+    sleep(random.randint(1, 2))
     data = await state.get_data()
 
     region = data.get("beer_region")
@@ -84,22 +92,27 @@ async def get_user_beer_price_high(message: types.Message, state: FSMContext):
     beer_style = data.get("beer_style")
     beer_price_low = data.get("beer_price_low")
     beer_price_high = data.get("beer_price_high")
+    low = min(beer_price_low, beer_price_high)
+    high = max(beer_price_low, beer_price_high)
 
-    await message.answer(f"Рекомендаций по вашему запросу: "
-          f"{collect_beer('count(*)', region, beer_type, beer_style, beer_price_low, beer_price_high)[0][0]}")
+    if collect_beer('count(*)', region, beer_type, beer_style, low, high)[0][0] == 0:
+        await message.answer("По вашему запросу ничего не найдено")
+    else:
+        await message.answer(f"Рекомендаций по вашему запросу: "
+                             f"{collect_beer('count(*)', region, beer_type, beer_style, low, high)[0][0]}")
 
-    call = collect_beer('*', region, beer_type, beer_style, beer_price_low, beer_price_high)
-    for i in range(len(call)):
-        beer_card = f"Название: {hbold(call[i][1])}\n" \
-                    f"Регион: {call[i][2]}\n" \
-                    f"Тип: {call[i][3]}\n" \
-                    f"Стиль: {call[i][4]}\n" \
-                    f"Крепость: {call[i][5]}\n" \
-                    f"Цена со скидкой: {call[i][6]}\n" \
-                    f"Цена без скидки: {call[i][7]}\n" \
-                    f"Объем: {call[i][8]}\n"
+        call = collect_beer('*', region, beer_type, beer_style, beer_price_low, beer_price_high)
+        for i in range(len(call)):
+            beer_card = f"Название: {hbold(call[i][1])}\n" \
+                        f"Регион: {call[i][2]}\n" \
+                        f"Тип: {call[i][3]}\n" \
+                        f"Стиль: {call[i][4]}\n" \
+                        f"Крепость: {call[i][5]}\n" \
+                        f"Цена со скидкой: {call[i][6]}\n" \
+                        f"Цена без скидки: {call[i][7]}\n" \
+                        f"Объем: {call[i][8]}\n"
 
-        await message.answer(beer_card)
+            await message.answer(beer_card)
 
 
 def main():
